76ac722fa4627591c3dcbe113ff596b93da40753
Imosed <jbell730@jbell730.webfactional.com>
2021-05-20 23:53:17 -0400

    Fixed some bugs and added a privacy policy page

--- Rutschig/wwwroot/js/site.js
+++ Rutschig/wwwroot/js/site.js
@@ -18,3 +18,9 @@ function getDomain(url) {
     let chunked = url.split('/');
     return chunked.slice(0, 3).join('/');
 }
+
+function getDateString(d = new Date()) {
+    return `${d.getFullYear()}-${(d.getMonth()+1).toString().padStart(2, '0')}-${d.getDate().toString().padStart(2, '0')}`
+}
+
+document.querySelector('#expiration').setAttribute('min', getDateString());
